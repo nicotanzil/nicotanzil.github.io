19dbff1eb6ec752eda53bcc95f104990bd803895
Clementius Nichklaus Tanzil <tanzilclementius@gmail.com>
2021-10-11 17:49:38 +0700

    Test commit

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-const App = () => {
-  return <div>My Portfolio</div>;
-};
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="test">
+      Portfolio
+      <div className="test2">asdasd</div>
+    </div>
+  );
+}
+
+export default App;
