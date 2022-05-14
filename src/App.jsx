2158920d3fcb5743203520e2552f94b76aaffa60
Clementius Nichklaus Tanzil <tanzilclementius@gmail.com>
2022-05-14 10:52:45 +0900

    v1 error fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import BackgroundParticles from "./components/BackgroundParticles";
-import { useEffect } from "react/cjs/react.development";
 import React, { useContext } from "react";
 import ThemeContext from "./store/theme-context";
 import Introduction from "./components/introduction/Introduction";
@@ -14,9 +13,6 @@ import ThemeToggle from "./components/theme-toggle/ThemeToggle";
 
 function App() {
 	const ctx = useContext(ThemeContext);
-
-	// initial useEffect
-	useEffect(() => {}, []);
 
 	return (
 		<div className="App" id="app-root" data-theme={ctx.theme}>
